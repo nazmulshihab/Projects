af49dc7e84d4de6929bd18d3f0c3b3a0abb356d9
shihab__ <nazmulhoquecsecu@gmail.com>
2025-09-20 20:51:56 +0600

    Updated app.js

--- Healthcare-app/server/src/app.js
+++ Healthcare-app/server/src/app.js
@@ -15,7 +15,7 @@ app.use("/",(req,res)=>{
 });
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`🚀 Server running on port ${process.env.PORT || 3000}`)
+  console.log(`Server running on port ${process.env.PORT || 3000}`)
 );
 
 export default app;
